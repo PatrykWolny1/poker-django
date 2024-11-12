1750f1431ad72559cfe27defeb019de6b2d20fd1
patryk <patrykwo@gmail.com>
2024-11-12 16:42:53 +0100

    change from django server to only daphne and redis servers

--- pokerweb/settings.py
+++ pokerweb/settings.py
@@ -16,6 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -142,12 +143,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-STATICFILES_DIRS = [
-    BASE_DIR / 'pokerweb/static/',
-]
+
+STATICFILES_DIRS = [BASE_DIR / "pokerweb/static/"] 
+
+# Static files for production (optional)
+STATIC_ROOT = BASE_DIR / "pokerweb/static/"
--- pokerweb/urls.py
+++ pokerweb/urls.py
@@ -16,10 +16,39 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
+from home import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('home.urls')),
+    
+    path('', views.index, name='home.index'),
+    path('cards_permutations/', views.cards_permutations, name='home.cards_permutations'),
+    path('one_pair_game/', views.one_pair_game, name='home.one_pair_game'),
+    
+    path('start_task_view/', views.start_task, name='start_task_view'),
+    path('stop_task_view/', views.stop_task, name='stop_task_view'), 
+    path('permutacje_view/', views.permutacje, name='permutacje_view'),
+    path('kombinacje_view/', views.kombinacje, name='kombinacje_view'),
+    path('high_card_view/', views.high_card, name='high_card_view'),
+    path('one_pair_view/', views.one_pair, name='one_pair_view'),
+    path('two_pairs_view/', views.two_pairs, name='two_pairs_view'),
+    path('three_of_a_kind_view/', views.three_of_a_kind, name='three_of_a_kind_view'),
+    path('straight_view/', views.straight, name='straight_view'),
+    path('color_view/', views.color, name='color_view'),
+    path('full_view/', views.full, name='full_view'),
+    path('carriage_view/', views.carriage, name='carriage_view'),
+    path('straight_flush_view/', views.straight_flush, name='straight_flush_view'),
+    path('straight_royal_flush_view/', views.straight_royal_flush, name='straight_royal_flush_view'),
+    path('download_saved_file/', views.download_saved_file, name='download_saved_file'),
+    path('submit_number/', views.submit_number, name='submit_number'),
 ]
+
+if settings.DEBUG:
+    print(settings.DEBUG, settings.STATIC_URL, settings.STATIC_ROOT, settings.BASE_DIR)
+    print(static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 

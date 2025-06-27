11e82ac379465d0a2d6c2befa654b9c3290871bd
Patryk <patrykwo@gmail.com>
2025-06-27 08:38:56 +0200

    update run_daphne; add export REDIS_URL and enable redis

--- run_daphne.sh
+++ run_daphne.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+source ~/django/bin/activate
+export REDIS_URL='redis://127.0.0.1:6379/1'
+sudo systemctl enable redis-server
 CUDA_VISIBLE_DEVICES="" daphne -e ssl:8000:privateKey=key.pem:certKey=cert.pem pokerweb.asgi:application
